88cddf2e8c5ea48c696c9c56e16985ce70626b6b
valeria-markish <84080359+valeria-markish@users.noreply.github.com>
2023-02-02 18:48:49 +0000

    Added the functionality for the select element

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 function update() {
   let lisbon = document.querySelector("#lisbon");
-  let lisbonDate = lisbon.querySelector(".date");
-  let lisbonTime = lisbon.querySelector(".time");
+  if (lisbon) {
+    let lisbonDate = lisbon.querySelector(".date");
+    let lisbonTime = lisbon.querySelector(".time");
 
-  lisbonDate.innerHTML = moment.tz("Europe/Lisbon").format("MMMM Do YYYY");
-  lisbonTime.innerHTML = moment
-    .tz("Europe/Lisbon")
-    .format("h:mm:ss [<small>]A[</small>]");
+    lisbonDate.innerHTML = moment.tz("Europe/Lisbon").format("MMMM Do YYYY");
+    lisbonTime.innerHTML = moment
+      .tz("Europe/Lisbon")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
 
   let barcelona = document.querySelector("#barcelona");
-  let barcelonaDate = barcelona.querySelector(".date");
-  let barcelonaTime = barcelona.querySelector(".time");
+  if (barcelona) {
+    let barcelonaDate = barcelona.querySelector(".date");
+    let barcelonaTime = barcelona.querySelector(".time");
 
-  barcelonaDate.innerHTML = moment.tz("Europe/Madrid").format("MMMM Do YYYY");
-  barcelonaTime.innerHTML = moment
-    .tz("Europe/Madrid")
-    .format("h:mm:ss [<small>]A[</small>]");
+    barcelonaDate.innerHTML = moment.tz("Europe/Madrid").format("MMMM Do YYYY");
+    barcelonaTime.innerHTML = moment
+      .tz("Europe/Madrid")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
 }
+
+function changeClock(event) {
+  if (event.target.value.length > 0) {
+    let cityName = event.target.value;
+    let cityNameTitle = cityName.replace("_", " ").split("/")[1];
+    let cities = document.querySelector("#cities");
+    cities.innerHTML = ` <div class="city">
+        <div class="header">
+          <h2>${cityNameTitle}</h2>
+          <p class="date">${moment.tz(cityName).format("MMMM Do YYYY")}</p>
+        </div>
+        <p class="time">${moment
+          .tz(cityName)
+          .format("h:mm:ss [<small>]A[</small>]")}</p>
+      </div>`;
+  }
+  setInterval(function () {
+    let cityDate = document.querySelector(".date");
+    let cityTime = document.querySelector(".time");
+    cityDate.innerHTML = moment.tz(event.target.value).format("MMMM Do YYYY");
+    cityTime.innerHTML = moment
+      .tz(event.target.value)
+      .format("h:mm:ss [<small>]A[</small>]");
+  }, 1000);
+}
+
 update();
 setInterval(update, 1000);
+
+let city = document.querySelector("#city");
+city.addEventListener("change", changeClock);
